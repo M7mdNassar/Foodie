fdf32d3abbebb4d461c4464e9e6a8afffeeffec8
Mohammed B Nassar <m.nassar4004@gmail.com>
2023-12-18 23:44:58 +0200

    set timer label

--- Foodie/Controllers/Profile/Chat/Message Cells/AudioIncomingMessageCell.swift
+++ Foodie/Controllers/Profile/Chat/Message Cells/AudioIncomingMessageCell.swift
@@ -8,30 +8,71 @@ class AudioIncomingMessageCell: UITableViewCell {
     
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var progressBar: UISlider!
+    @IBOutlet weak var timeLabel: UILabel!
     
+    var totalDuration: TimeInterval = 0
+
     var audioURL: URL?
 
     @IBAction func playButtonTapped(_ sender: UIButton) {
-          if let audioURL = audioURL {
-              AudioManager.shared.playAudio(at: audioURL, updateProgress: { [weak self] (progress) in
-                  // Update progress bar here
-                  self?.progressBar.value = Float(progress)
-              })
-          }
-      }
+            if let audioURL = audioURL {
+                AudioManager.shared.getAudioDuration(at: audioURL) { [weak self] (totalDuration) in
+                    guard let totalDuration = totalDuration else {
+                        // Handle error
+                        return
+                    }
+
+                    // Store total duration in the cell
+                    self?.totalDuration = totalDuration
+
+                    // Update UI with total duration (you may want to format it)
+                    let totalDurationString = self?.formatTime(seconds: Int(totalDuration))
+                    self?.timeLabel.text = totalDurationString
+
+                    // Now you can initiate playback
+                    AudioManager.shared.playAudio(at: audioURL,
+                                                  updateProgress: { [weak self] (progress) in
+                                                      // Update progress bar here
+                                                      self?.progressBar.value = Float(progress)
+                                                  },
+                                                  updateTimeLabel: { [weak self] (timeString) in
+                                                      // Update time label here
+                                                      self?.timeLabel.text = timeString
+                                                  })
+                }
+            }
+        }
+
+        // Helper method to format time in seconds as mm:ss
+        private func formatTime(seconds: Int) -> String {
+            let minutes = seconds / 60
+            let seconds = seconds % 60
+            return String(format: "%02d:%02d", minutes, seconds)
+        }
     
     // MARK: - Configuration
     func configure(audioURL: URL, userImageUrl: String) {
-        self.backgroundColor = .clear
-        self.backgroundMessage.backgroundColor = .foodieLightBlue
-        self.backgroundMessage.layer.cornerRadius = 15.0
-        self.backgroundMessage.layer.masksToBounds = true
+           self.backgroundColor = .clear
+           self.backgroundMessage.backgroundColor = .foodieLightBlue
+           self.backgroundMessage.layer.cornerRadius = 15.0
+           self.backgroundMessage.layer.masksToBounds = true
+           self.progressBar.value = 0
+           self.audioURL = audioURL
 
-        self.progressBar.value = 0.0
-        self.audioURL = audioURL
+           loadUserImage(urlString: userImageUrl)
 
-        loadUserImage(urlString: userImageUrl)
-    }
+           // Get audio duration and update the time label
+           AudioManager.shared.getAudioDuration(at: audioURL) { [weak self] (totalDuration) in
+               guard let totalDuration = totalDuration else {
+                   // Handle error
+                   return
+               }
+
+               // Update UI with total duration (you may want to format it)
+               let totalDurationString = self?.formatTime(seconds: Int(totalDuration))
+               self?.timeLabel.text = totalDurationString
+           }
+       }
     
     func loadUserImage(urlString: String) {
         DispatchQueue.global(qos: .userInitiated).async {
@@ -47,3 +88,5 @@ class AudioIncomingMessageCell: UITableViewCell {
         }
     }
 }
+
+
--- Foodie/Model/AudioManager.swift
+++ Foodie/Model/AudioManager.swift
@@ -6,6 +6,9 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
     static let shared = AudioManager()
     var currentRecordingURL: URL?
     private var updateProgressTimer: Timer?
+    private var updateTimeLabelTimer: Timer?
+
+    private var totalDuration: TimeInterval = 0
 
     private var audioRecorder: AVAudioRecorder?
     private var audioPlayer: AVAudioPlayer?
@@ -52,28 +55,59 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
         audioRecorder?.stop()
     }
 
-    func playAudio(at url: URL, updateProgress: @escaping (Double) -> Void) {
-            do {
-                audioPlayer = try AVAudioPlayer(contentsOf: url)
-                audioPlayer?.delegate = self
-                audioPlayer?.prepareToPlay()
-                audioPlayer?.play()
+    func playAudio(at url: URL, updateProgress: @escaping (Double) -> Void, updateTimeLabel: @escaping (String) -> Void) {
+           do {
+               audioPlayer = try AVAudioPlayer(contentsOf: url)
+               audioPlayer?.delegate = self
+               audioPlayer?.prepareToPlay()
 
-                // Schedule a timer to update the progress every 0.1 seconds
-                updateProgressTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { [weak self] (timer) in
-                    guard let strongSelf = self, let player = strongSelf.audioPlayer else { return }
-                    let progress = player.currentTime / player.duration
-                    updateProgress(progress)
-                })
-            } catch {
-                print("Error playing audio: \(error.localizedDescription)")
-            }
-        }
+               // Store the total duration
+               totalDuration = audioPlayer?.duration ?? 0
+
+               audioPlayer?.play()
+
+               // Schedule a timer to update the progress and time label every 0.1 seconds
+               updateTimeLabelTimer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true, block: { [weak self] (timer) in
+                   guard let strongSelf = self, let player = strongSelf.audioPlayer else { return }
+                   let progress = player.currentTime / strongSelf.totalDuration
+                   updateProgress(progress)
+
+                   // Calculate the remaining time dynamically
+                   let remainingTime = strongSelf.totalDuration - player.currentTime
+                   let remainingTimeString = strongSelf.formatTime(seconds: Int(remainingTime))
+                   updateTimeLabel(remainingTimeString)
+               })
+           } catch {
+               print("Error playing audio: \(error.localizedDescription)")
+           }
+       }
+    
+    func getAudioDuration(at url: URL, completion: @escaping (TimeInterval?) -> Void) {
+         do {
+             audioPlayer = try AVAudioPlayer(contentsOf: url)
+             audioPlayer?.delegate = self
+             audioPlayer?.prepareToPlay()
+
+             // Obtain the total duration
+             let totalDuration = audioPlayer?.duration
+             completion(totalDuration)
+         } catch {
+             print("Error getting audio duration: \(error.localizedDescription)")
+             completion(nil)
+         }
+     }
     
     func stopAudio() {
-           audioPlayer?.stop()
-           updateProgressTimer?.invalidate()
-       }
+        audioPlayer?.stop()
+        updateTimeLabelTimer?.invalidate()
+    }
+
+    // Helper method to format time in seconds as mm:ss
+    private func formatTime(seconds: Int) -> String {
+        let minutes = seconds / 60
+        let seconds = seconds % 60
+        return String(format: "%02d:%02d", minutes, seconds)
+    }
 
     // MARK: - AVAudioRecorderDelegate
 
@@ -93,4 +127,3 @@ class AudioManager: NSObject, AVAudioRecorderDelegate, AVAudioPlayerDelegate {
          }
          updateProgressTimer?.invalidate()
      }}
-
